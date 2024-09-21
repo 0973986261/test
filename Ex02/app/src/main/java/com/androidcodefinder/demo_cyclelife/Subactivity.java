9ed3061e54b7e0dbe72d068e1869c644a9bb5246
0973985261 <lichjj2002@gmail.com>
2024-09-21 18:33:40 +0700

    add Ex1 2 3

---
+++ Ex02/app/src/main/java/com/androidcodefinder/demo_cyclelife/Subactivity.java
@@ -0,0 +1,22 @@
+package com.androidcodefinder.demo_cyclelife;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Subactivity extends AppCompatActivity {
+    Button btnok;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subactivity);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
